447314725be9621742e7e1bba34383b0685d860c
Jackson Moore <jkmoore1231@gmail.com>
2022-09-19 16:45:10 -0700

    Added review model and val schema

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const path = require('path');
 //Defining mongoose which provides tools to allow us to model our application data in MongoDB.
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-//loading the campgroundSchema validation schema.
-const {campgroundSchema} = require('./validationSchemas.js');
+//loading the campgroundSchema and reviewSchema validation schemas.
+const {campgroundSchema, reviewSchema } = require('./validationSchemas.js');
 const wrapAsync = require('./utils/wrapAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 //This is our Campground model
 const Campground = require('./models/campground.js');
+//This is our Review model
+const Review = require('./models/review.js');
+const { findByIdAndDelete } = require('./models/review.js');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(function(){
     console.log("Connection Open");
@@ -45,6 +48,22 @@ const validateCampground = function(req, res, next){
 
 }
 
+//review validation middleware
+const validateReview = function(req, res, next){
+    //This line is using the schema to validate the body and save the error if present.
+    const { error } = reviewSchema.validate(req.body);
+    //This is checking if there is an error to throw and express error with the error details and a status code
+    if(error){
+        //Details is an array of objects, so we are mapping over this and joining into a single string comma separated.
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg , 400)
+    } else{
+        next();
+    }
+
+}
+
+
 app.get('/', function(req, res){
     res.render('home')
 })
@@ -68,7 +87,7 @@ app.post('/campgrounds', validateCampground, wrapAsync(async function(req, res, 
     }))
 
 app.get('/campgrounds/:id', wrapAsync(async function(req, res){
-        const campground = await Campground.findById(req.params.id);
+        const campground = await Campground.findById(req.params.id).populate('reviews');
         res.render('campgrounds/show', {campground});
 }))
 
@@ -85,10 +104,33 @@ app.put('/campgrounds/:id', validateCampground, wrapAsync(async function(req, re
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+//This route is deleting a specific campground from the database.
 app.delete('/campgrounds/:id', wrapAsync(async function(req, res){
     const {id} = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds/');
+}))
+
+//This route add a new review to specified campground
+app.post('/campgrounds/:id/reviews', validateReview, wrapAsync(async function(req, res){
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+
+}))
+
+//This route deletes a specific review from the selected campground
+app.delete('/campgrounds/:id/reviews/:reviewId', wrapAsync(async function(req, res){
+    //This line has destructured the id and reviewId from req.params
+    const { id, reviewId } = req.params;
+    //This line we are waiting for the review matching the reviewId to be removed from the specific campground
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId }});
+    //This line we are waiting for the specific review to be deleted from the database.
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', function(req, res, next){
