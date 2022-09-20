ce62dc668fc2dc422fb0a8da272094fc15ade55b
Jackson Moore <jkmoore1231@gmail.com>
2022-09-20 16:36:39 -0700

    Added Review validation schema

--- validationSchemas.js
+++ validationSchemas.js
@@ -1,4 +1,5 @@
 //Defining joi which is a schema description language and data validator.
+const { number, string } = require('joi');
 const Joi = require('joi');
 
 //Joi schema for Campground data validation.
@@ -10,5 +11,14 @@ module.exports.campgroundSchema = Joi.object({
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required(),
+    }).required()
+})
+
+//Joi schema for Review data validation
+module.exports.reviewSchema = Joi.object({
+    //Review is the object on the show.ejs page and we check that it is present (required)
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
     }).required()
 })
